99829f13a856774a7b941673829a3914a2783f41
jtomori <jtomori@users.noreply.github.com>
2024-09-03 14:11:19 +0200

    Block failing domain

--- check_links.py
+++ check_links.py
@@ -10,7 +10,7 @@ from concurrent.futures import ThreadPoolExecutor
 urllib3.disable_warnings()
 
 AGENT = os.getenv("AGENT", "Mozilla/5.0 ()")
-SKIPLIST = ["https://linkedin.com", "https://linux.die.net"]
+SKIPLIST = ["https://linkedin.com", "https://linux.die.net", "https://code.visualstudio.com"]
 
 
 def get_status(link):
