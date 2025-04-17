f5469e34d57213a36fe938de3ed97a038c8ded06
jtomori <jtomori@users.noreply.github.com>
2025-04-17 14:18:01 +0200

    Ignore checking UE's links

--- check_links.py
+++ check_links.py
@@ -10,7 +10,7 @@ from concurrent.futures import ThreadPoolExecutor
 urllib3.disable_warnings()
 
 AGENT = os.getenv("AGENT", "Mozilla/5.0 ()")
-SKIPLIST = ["https://linkedin.com", "https://linux.die.net", "https://code.visualstudio.com", "https://dl.acm.org"]
+SKIPLIST = ["https://linkedin.com", "https://linux.die.net", "https://code.visualstudio.com", "https://dl.acm.org", "https://www.unrealengine.com"]
 
 
 def get_status(link):
